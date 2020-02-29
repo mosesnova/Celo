f7cb14161e00043e2d7c46d6b721e2b2b5585d80
Moses Nova <mosesnova@gmail.com>
2020-02-29 13:12:42 +1300

    Initial Commit
    Commit

---
+++ CeloWebApplication/Models/CeloContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CeloWebApplication.Models
+{
+    public partial class CeloContext : DbContext
+    {
+        public CeloContext()
+        {
+        }
+
+        public CeloContext(DbContextOptions<CeloContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=Celo;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.DateOfBirth).HasColumnType("date");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
